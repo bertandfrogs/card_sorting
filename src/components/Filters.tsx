a3ec75a2dc50f39da76553901199771f4efd6050
davidmolgard <molgarddavid@gmail.com>
2025-02-12 15:09:11 -0700

    added additional values to filters

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -9,6 +9,8 @@ const Filters: React.FC = () => {
           <option value="">All</option>
           <option value="100">100 Level</option>
           <option value="200">200 Level</option>
+          <option value="300">300 Level</option>
+          <option value="400">400 Level</option>
         </select>
       </div>
 
@@ -24,8 +26,10 @@ const Filters: React.FC = () => {
         <label>Credit Hours</label>
         <select>
           <option value="">All</option>
-          <option value="3">3.0 Credits</option>
           <option value="0.5">0.5 Credits</option>
+          <option value="2">2.0 Credits</option>
+          <option value="3">3.0 Credits</option>
+          <option value="4">4.0 Credits</option>
         </select>
       </div>
 
@@ -35,6 +39,8 @@ const Filters: React.FC = () => {
           <option value="">All</option>
           <option value="Fall">Fall</option>
           <option value="Winter">Winter</option>
+          <option value="Spring">Spring</option>
+          <option value="Summer">Summer</option>
         </select>
       </div>
     </div>
